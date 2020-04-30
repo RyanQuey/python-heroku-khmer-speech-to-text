cf788a6aaee3d40f32b429af3c91f9c8aaece7f3
Ryan Quey <rlquey2@gmail.com>
2020-04-30 12:05:46 +0000

    can wakeup server while uploading file with new endpoint

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.http import HttpResponseServerError
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import path, include
 
 from django.contrib import admin
@@ -18,5 +21,7 @@ urlpatterns = [
     path("request-transcribe/", hello.views.transcribe, name="transcribe"),
     path("resume-request/", hello.views.resume_request, name="resume-request"),
     path("check-status/", hello.views.check_status, name="check-status"),
+    # something to add for when using heroku hobby dynos
+    path("wake-up/", csrf_exempt(lambda request: HttpResponse('Hello World! Waking up')), name="wake-up"),
     path("admin/", admin.site.urls),
 ]
