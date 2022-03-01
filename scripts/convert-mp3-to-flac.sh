a6fae9b99a8075e64386517abbf168f46c92e56c
Ryan Quey <quey.ryan@gmail.com>
2022-03-02 03:31:20 +0700

    add mp3 to flac bash script

---
+++ scripts/convert-mp3-to-flac.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# takes a directory full of mp3s and converts to flac
+# not recursive (though easily could be made to do so)
+
+##################
+# Instructions
+# unzip first, e.g,. mkdir -p ./mp3s_dir && unzip files.zip -d ./mp3s_dir
+# e.g., ./convert-mp3-to-flac.sh ./mp3s_dir ./flacs_dir
+
+# relative or abs path to  where the mp3s live
+src_dir=$1
+
+# relative or abs path to where the flacs will live
+target_dir=$2
+
+# creating target dir if not exists
+mkdir -p $target_dir
+
+# https://stackoverflow.com/a/965072/6952495
+for filepath  in $src_dir/*.mp3; do
+	filename_with_ext=$(basename -- "$filepath")
+	filename="${filename_with_ext%.*}"
+	extension="${filename_with_ext##*.}"
+	echo "========================="
+	echo ""
+	printf "\nfilename: $filename"
+	printf "\nfilename_with_ext: $filename_with_ext"
+	printf "\nextension: $extension"
+	ffmpeg -i "$filepath" "$target_dir/${filename}.flac"
+done
